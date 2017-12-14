f097903d2bc932ee0fb2f9fbc1f6a67835755e7d
sakib3 <sakib3@gmail.com>
2017-12-14 21:30:41 +0100

    api has  token store service

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Storage } from '@ionic/storage';
 
 /*
   Generated class for the ApiProvider provider.
@@ -12,14 +13,24 @@ import 'rxjs/add/operator/map';
 export class Api {
   //url: string = "https://shareridebd.herokuapp.com/api";
   url: string = "http://localhost:8000";
-  constructor(public http: Http) {
+  token: string = null;
+  constructor(public http: Http,private storage: Storage) {
+    //this.init();
   }
-
+  init() {
+    var self = this;
+    this.storage.get('token').then((val) => {
+      console.log('Token set into api service', val);
+      self.token = val;
+    });
+  }
   get(endPoint: string, params?: any, reqOpts?:any){
+    console.log('Get with token: ', this.token);
     return this.http.get(this.url + '/' + endPoint).map(res => res.json());
   }
 
   post(endpoint: string, body: any, reqOpts?: any) {
+    console.log('Post with token: ', this.token);
     return this.http.post(this.url + '/' + endpoint, body, reqOpts).map(res => res.json());
   }
 }
